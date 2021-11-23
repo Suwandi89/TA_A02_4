cc61e461f0920e2ca36b6b8f8928d6819c1e9abe
Suwandi Kurniawan <suwandikurniawan89@gmail.com>
2021-11-23 16:23:46 +0700

    feat: init push

---
+++ sibusiness/src/test/java/apap/tugaskelompok/sibusiness/SibusinessApplicationTests.java
@@ -0,0 +1,13 @@
+package apap.tugaskelompok.sibusiness;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SibusinessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
