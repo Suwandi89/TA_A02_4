8995a33a1aef44c9055ae1cfaa1e415688d19146
Trisetio Putra <trisetiopp@gmail.com>
2021-11-25 17:08:01 +0700

    home screen

---
+++ src/main/java/apap/tugaskelompok/sibusiness/controller/PageController.java
@@ -0,0 +1,16 @@
+package apap.tugaskelompok.sibusiness.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import java.security.Principal;
+
+@Controller
+public class PageController {
+    @RequestMapping("/")
+    private String home(HttpServletRequest request, Model model) {
+        return "home";
+    }
+}
