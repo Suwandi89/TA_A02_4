aa3e08224850ee33404a5e4c28ad9b3941af600a
mhalimss <muhammad.halim91@ui.ac.id>
2021-11-27 00:38:26 +0700

    fitur5

--- src/main/java/apap/tugaskelompok/sibusiness/controller/UserController.java
+++ src/main/java/apap/tugaskelompok/sibusiness/controller/UserController.java
@@ -2,9 +2,12 @@ package apap.tugaskelompok.sibusiness.controller;
 
 import apap.tugaskelompok.sibusiness.models.RoleModel;
 import apap.tugaskelompok.sibusiness.models.UserModel;
+import apap.tugaskelompok.sibusiness.repository.UserDB;
 import apap.tugaskelompok.sibusiness.service.RoleService;
 import apap.tugaskelompok.sibusiness.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,6 +26,9 @@ public class UserController {
     @Autowired
     private RoleService roleService;
 
+    @Autowired
+    private UserDB userDB;
+
     @GetMapping(value = "/add")
     private String addUserFormPage(Model model){
         UserModel user = new UserModel();
@@ -39,4 +45,15 @@ public class UserController {
         return "redirect:/";
     }
 
+    @GetMapping(value = "/list")
+    private String getListRole(Model model){
+        List<UserModel> listUser = userDB.findAll();
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        String role =auth.getAuthorities().toString().replace("[", "").replace("]","");
+        if(role.equals("Manager Business")){
+            model.addAttribute("listUser", listUser);
+            return "list-user";
+        }
+        return "error/403";
+    }
 }
