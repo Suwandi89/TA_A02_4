5e042645de81e272abfdd1006e3f1f46d89263d4
Trisetio Putra <trisetiopp@gmail.com>
2021-12-06 21:50:29 +0700

    cek

--- src/main/java/apap/tugaskelompok/sibusiness/security/WebSecurityConfig.java
+++ src/main/java/apap/tugaskelompok/sibusiness/security/WebSecurityConfig.java
@@ -21,7 +21,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
                 .antMatchers("/css/**").permitAll()
                 .antMatchers("/jss/**").permitAll()
-//                .antMatchers("/user/add").hasAuthority("Manager Business")
+                .antMatchers("/user/add").hasAuthority("Manager Business")
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
@@ -34,19 +34,19 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     public BCryptPasswordEncoder encoder() { return new BCryptPasswordEncoder(); }
 
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .passwordEncoder(encoder())
-                .withUser("kijangSatu").password(encoder().encode("nasiGoreng"))
-                .roles("Manager Business");
-    }
-
 //    @Autowired
-//    private UserDetailsService userDetailsService;
-//
-//    @Autowired
-//    public void configAuthentication(AuthenticationManagerBuilder auth) throws Exception {
-//        auth.userDetailsService(userDetailsService).passwordEncoder(encoder());
+//    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.inMemoryAuthentication()
+//                .passwordEncoder(encoder())
+//                .withUser("kijangSatu").password(encoder().encode("nasiGoreng"))
+//                .roles("Manager Business");
 //    }
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Autowired
+    public void configAuthentication(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(encoder());
+    }
 }
