4990eb3f780af82a3685a5d09c1af73f4a0f0331
Trisetio Putra <trisetiopp@gmail.com>
2021-12-23 19:26:20 +0700

    feat 10

--- src/main/java/apap/tugaskelompok/sibusiness/repository/CouponDB.java
+++ src/main/java/apap/tugaskelompok/sibusiness/repository/CouponDB.java
@@ -5,7 +5,10 @@ import apap.tugaskelompok.sibusiness.models.CouponModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CouponDB extends JpaRepository<CouponModel,Long> {
+    Optional<CouponModel> findByIdCoupon(Long idCoupon);
 
 }
